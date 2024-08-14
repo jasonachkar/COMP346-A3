40f94317b26c3de16c526b53cf9f77bc4e556c7f
Jason Achkar Diab <ja_achka@live.concordia.ca>
2024-08-14 16:13:42 -0400

    Final Commit

--- Monitor.java
+++ Monitor.java
@@ -5,7 +5,6 @@ public class Monitor {
 	private final int numOfPhilosophers;
 	private boolean isTalking = false; // Tracks if any philosopher is currently talking
 
-
 	// Constructor
 	public Monitor(int numOfPhilosophers) {
 		this.numOfPhilosophers = numOfPhilosophers;
@@ -19,10 +18,10 @@ public class Monitor {
 
 	// Request to talk
 	public void requestTalk(int piTID) {
-		synchronized (self[piTID]) {
+		synchronized (this) {
 			while (isTalking || state[piTID] == State.EATING) {
 				try {
-					self[piTID].wait();
+					wait();
 				} catch (InterruptedException e) {
 					Thread.currentThread().interrupt();
 				}
@@ -33,34 +32,34 @@ public class Monitor {
 
 	// End the talk
 	public void endTalk(int piTID) {
-		synchronized (self[piTID]) {
+		synchronized (this) {
 			isTalking = false;
-			self[piTID].notifyAll();
+			notifyAll(); // Notify all waiting philosophers
 		}
 	}
 
 	// Pick up the chopsticks
-	public void pickUp(int piTID) {
-		synchronized (self[piTID]) {
-			state[piTID] = State.HUNGRY;
-			test(piTID);
-			while (state[piTID] != State.EATING) {
+	public void pickUp(int philosopherId) {
+		synchronized (self[philosopherId]) {
+			state[philosopherId] = State.HUNGRY;
+			test(philosopherId); // Test if the philosopher can start eating
+			while (state[philosopherId] != State.EATING) {
 				try {
-					self[piTID].wait();
+					self[philosopherId].wait(); // Wait until the philosopher can eat
 				} catch (InterruptedException e) {
-					Thread.currentThread().interrupt();
+					Thread.currentThread().interrupt(); // Re-interrupt the thread
 				}
 			}
 		}
 	}
 
 	// Put down the chopsticks
-	public void putDown(int piTID) {
-		synchronized (self[piTID]) {
-			state[piTID] = State.THINKING;
-			// Test left and right neighbors
-			test((piTID + numOfPhilosophers - 1) % numOfPhilosophers); // Left philosopher
-			test((piTID + 1) % numOfPhilosophers); // Right philosopher
+	public void putDown(int philosopherId) {
+		synchronized (self[philosopherId]) {
+			state[philosopherId] = State.THINKING;
+			// Test if the left and right neighbors can start eating
+			test((philosopherId + numOfPhilosophers - 1) % numOfPhilosophers);
+			test((philosopherId + 1) % numOfPhilosophers);
 		}
 	}
 
@@ -68,10 +67,11 @@ public class Monitor {
 	private void test(int piTID) {
 		int left = (piTID + numOfPhilosophers - 1) % numOfPhilosophers;
 		int right = (piTID + 1) % numOfPhilosophers;
-		if (state[left] != State.EATING && state[piTID] == State.HUNGRY && state[right] != State.EATING) {
-			state[piTID] = State.EATING;
-			synchronized (self[piTID]) {
-				self[piTID].notify(); // Notify the current philosopher they can eat
+
+		synchronized (self[piTID]) {
+			if (state[piTID] == State.HUNGRY && state[left] != State.EATING && state[right] != State.EATING) {
+				state[piTID] = State.EATING;
+				self[piTID].notify(); // Notify the philosopher that they can eat
 			}
 		}
 	}
